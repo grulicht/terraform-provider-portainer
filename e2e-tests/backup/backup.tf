7a94ef3e5dd9f9b763fd40c83830dff17cc9f282
grulicht <to.grulich@gmail.com>
2025-03-29 21:57:54 +0100

    chore(e2e): add daily E2E test workflow and initial test case for user resource

--- e2e-tests/backup/backup.tf
+++ e2e-tests/backup/backup.tf
@@ -1,4 +1,4 @@
 resource "portainer_backup" "snapshot" {
   password    = var.portainer_backup_password
-  output_path = var.portainer_backup_outputh_path
+  output_path = var.portainer_backup_output_path
 }
