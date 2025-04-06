097d02881a6693cc1205414c5159f787afce1ec6
grulicht <to.grulich@gmail.com>
2025-04-06 11:11:31 +0200

    chore: patch endpoints snapshot

--- internal/resource_endpoints_snapshot.go
+++ internal/resource_endpoints_snapshot.go
@@ -27,19 +27,27 @@ func resourceEndpointsSnapshot() *schema.Resource {
 func resourceEndpointsSnapshotCreate(d *schema.ResourceData, meta interface{}) error {
 	client := meta.(*APIClient)
 
-	var url string
-	var id string
+	var req *http.Request
+	var err error
 
 	if v, ok := d.GetOk("endpoint_id"); ok {
-		eid := v.(int)
-		url = fmt.Sprintf("%s/endpoints/%d/snapshot", client.Endpoint, eid)
-		id = strconv.Itoa(eid)
+		id := v.(int)
+		req, err = http.NewRequest("POST", fmt.Sprintf("%s/endpoints/%d/snapshot", client.Endpoint, id), nil)
+		if err != nil {
+			return err
+		}
+		d.SetId(strconv.Itoa(id))
 	} else {
-		url = fmt.Sprintf("%s/endpoints/snapshot", client.Endpoint)
-		id = "all"
+		req, err = http.NewRequest("POST", fmt.Sprintf("%s/endpoints/snapshot", client.Endpoint), nil)
+		if err != nil {
+			return err
+		}
+		d.SetId("all")
 	}
 
-	resp, err := client.DoRequest(http.MethodPost, url, nil, nil)
+	req.Header.Set("X-API-Key", client.APIKey)
+
+	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
 		return err
 	}
@@ -49,7 +57,6 @@ func resourceEndpointsSnapshotCreate(d *schema.ResourceData, meta interface{}) e
 		return fmt.Errorf("failed to snapshot endpoint(s): HTTP %d", resp.StatusCode)
 	}
 
-	d.SetId(id)
 	return nil
 }
 
