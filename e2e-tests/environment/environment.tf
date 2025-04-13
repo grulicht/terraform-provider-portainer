59646845c3c6f63b291f39934205906d415ddf9c
grulicht <to.grulich@gmail.com>
2025-04-13 19:35:03 +0200

    feat: add Kubernetes volume and storage resources with examples and docs plus add Kubertenes resources added to e2e daily tests

--- e2e-tests/environment/environment.tf
+++ e2e-tests/environment/environment.tf
@@ -2,4 +2,8 @@ resource "portainer_environment" "your-host" {
   name                = var.portainer_environment_name
   environment_address = var.portainer_environment_address
   type                = var.portainer_environment_type
+
+  tls_enabled            = var.tls_enabled
+  tls_skip_verify        = var.tls_skip_verify
+  tls_skip_client_verify = var.tls_skip_client_verify
 }
